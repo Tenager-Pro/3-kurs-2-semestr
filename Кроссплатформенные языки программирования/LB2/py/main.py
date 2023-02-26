255aecff08b945ed19368657c966ba9f1e7d687c
Tenager-Pro <65409198+Tenager-Pro@users.noreply.github.com>
2023-02-26 19:34:09 +0300

    Добавление в КРПО

--- Кроссплатформенные языки программирования/LB2/py/main.py
+++ Кроссплатформенные языки программирования/LB2/py/main.py
@@ -11,9 +11,16 @@ from patient_db import *
 from contract_db import *
 from doctor_db import *
 from patient_card_db import *
+from Menu import *
+
+class Menu(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
 if __name__ == "__main__":
-    get_data_patient()
-    get_data_contract()
-    get_data_doctor()
-    get_data_patient_card()
+    name = ""
+    app = QtWidgets.QApplication(sys.argv)
+    menu = Menu()
+    menu.show()
